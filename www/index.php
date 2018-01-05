58d5708a295f723be247514559e4705fae229422
PoweredByPeople <louis.capellaedu@gmail.com>
2018-01-04 22:23:00 -0600

    Update index.php

--- www/index.php
+++ www/index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!!!! Whaddup Doh????????"; ?></h1>
+	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!!!! Just hack the dns for now?"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
