29ae496a5446f169414b53b9e81d0ebd8b155e83
Louis <louis@poweredbypeople.me>
2018-01-16 22:03:37 -0600

    chapter 5 big commit

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 from flask_login import UserMixin
-from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +19,10 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -29,6 +32,3 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
